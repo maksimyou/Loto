c1b9487cd78dea0250e2b581e44397350f413c3a
Maksim Asmarin <maksimyou@gmail.com>
2024-02-06 00:47:10 +0300

    fix server

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const path = require('path')
+const { fileURLToPath } = require('url')
+const hostname = '89.104.66.35';
+
+const PORT = 5020;
+
+const app = express()
+
+app.use(express.static(__dirname))
+
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'))
+})
+app.listen(PORT, hostname)
